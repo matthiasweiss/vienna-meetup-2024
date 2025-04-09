c514f61a1da449b274def8219f9c19008a462451
Matthias Weiß <mail@matthiasweiss.at>
2025-04-09 07:20:52 +0200

    adapt

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,13 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Data\StorePostData;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
     public function store(StorePostData $data): RedirectResponse
     {
-        $user = Auth::user();
+        /** @var User $user */
+        $user = auth()->user();
 
         $user->posts()->create($data->toArray());
 
